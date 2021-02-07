8f70f9be3a0ac53dfd6ee2d7b17160c7081eefb7
Thomas Kercheval <spacekattpoispin@gmail.com>
2021-02-07 00:11:33 -0800

    add basic lifecycle implementation

--- apps/minesweeper/src/App.tsx
+++ apps/minesweeper/src/App.tsx
@@ -1,10 +1,6 @@
 import React, { FunctionComponent, useState } from "react";
 import "./App.css";
-
-export interface MinefieldProps {}
-export interface MinefieldState {
-  field: number[][];
-}
+import { v4 } from "uuid";
 
 const fieldSize = 16;
 const numberOfMines = 8;
@@ -29,22 +25,91 @@ export interface MineProps {
   x: number;
   y: number;
   isMine: boolean;
+  neighbors: number;
+  mineHandler: () => void;
 }
-// export const Mines: FunctionComponent<MineProps>
+
+export interface MineState {
+  flagged: boolean;
+  hidden: boolean;
+}
+
+export const Mine: FunctionComponent<MineProps> = (props) => {
+  const [state, setState] = useState<MineState>({
+    flagged: false,
+    hidden: true,
+  });
+  const handleClick = (event: any): void => {
+    if (!state.hidden) {
+      return;
+    }
+
+    if (event.shiftKey) {
+      setState({ flagged: true, hidden: true });
+      return;
+    }
+
+    if (state.flagged) {
+      return;
+    }
+
+    if (props.isMine) {
+      props.mineHandler();
+    }
+  };
+  return (
+    <button className="Mine" onClick={handleClick}>
+      {/* <p>{props.isMine ? "x" : "o"}</p> */}
+    </button>
+  );
+};
+
+export interface MinefieldProps {}
+export interface MinefieldState {
+  gameOver: boolean;
+  field: number[][];
+}
 
 export const Minefield: FunctionComponent<MinefieldProps> = ({}) => {
-  const [state, setState] = useState({ field: initialField });
+  const [state, setState] = useState({ field: initialField, gameOver: false });
   const mines = [];
-  //  for (const i = 0; i < state.field.length; i++) {
-
-  // };
-  return <div className="Minefield"> style="1 0 {100 / (fieldSize + 8)}%"</div>;
+  for (let i = 0; i < state.field.length; i++) {
+    for (let j = 0; j < state.field[i].length; j++) {
+      const mineOpts = {
+        x: i,
+        y: j,
+        isMine: true,
+        neighbors: 0,
+        mineHandler: () => {
+          setState({ gameOver: true, field: state.field });
+        },
+      };
+      mines.push(<Mine key={v4()} {...mineOpts} />);
+    }
+  }
+  const onClick = (event: any) => {
+    setState({ gameOver: false, field: initialField });
+  };
+  return state.gameOver ? (
+    <div className="GameOver">
+      <button className="GameOverButton" onClick={onClick}>
+        Retry?
+      </button>
+    </div>
+  ) : (
+    <div className="Minefield">{mines}</div>
+  );
 };
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
+    <div className="AppContainer">
+      <div className="App">
+        <div className="MinefieldContainer">
+          <header className="App-header"></header>
+          <Minefield />
+        </div>
+      </div>
     </div>
   );
 }
