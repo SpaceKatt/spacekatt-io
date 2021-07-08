f1376cc0a5432d6249a329ed755782988acae7a7
SpaceKatt <spacekattpoispin@gmail.com>
2021-07-08 11:41:39 -0700

    add website meta

--- spacekatt-io/sidebars/sidebars.js
+++ spacekatt-io/sidebars/sidebars.js
@@ -1,5 +1,5 @@
 module.exports = {
   artSidebar: ['art', 'bottom-text', 'spacekatt-logo'],
   techSidebar: ['tech', 'minesweeper', 'esp32-weather-telemetry-station'],
-  aboutSidebar: ['about'],
+  aboutSidebar: ['about', 'website-meta'],
 };
