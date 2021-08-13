74b3f6d9217c0fe51d7caac7fb5c0b7017a7ac81
SpaceKatt <spacekattpoispin@gmail.com>
2021-08-13 09:17:22 -0700

    add test and lint config

---
+++ apps/population-sim/.eslint.js
@@ -0,0 +1,27 @@
+// This is a workaround for https://github.com/eslint/eslint/issues/3458
+require('@rushstack/eslint-config/patch/modern-module-resolution');
+
+module.exports = {
+  extends: ['@rushstack/eslint-config/profile/node'],
+  parserOptions: { tsconfigRootDir: __dirname },
+  rules: {
+    '@typescript-eslint/naming-convention': [
+      // Do not start interfaces with prefix "I"
+      'error',
+      {
+        selector: 'interface',
+        format: ['PascalCase'],
+        custom: {
+          regex: '^I[A-Z]',
+          match: false,
+        },
+      },
+    ],
+    '@typescript-eslint/explicit-member-accessibility': [
+      'error',
+      {
+        accessibility: 'no-public',
+      },
+    ],
+  },
+};
