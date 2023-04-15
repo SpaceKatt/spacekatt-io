f12ead09db86fc3b819e474087eec588a261b442
SpaceKatt <spacekattpoispin@gmail.com>
2023-04-15 14:30:55 -0700

    update home features

--- spacekatt-io/src/components/generics/feature.tsx
+++ spacekatt-io/src/components/generics/feature.tsx
@@ -1,27 +1,68 @@
 import React from 'react';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 
-import styles from '../../pages/styles.module.css';
 import Link from '@docusaurus/Link';
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Typography,
+} from '@mui/material';
 
 export interface FeatureProps {
-  imageUrl: string,
-  title: string,
-  description: string,
+  imageUrl: string;
+  title: string;
+  description: string;
+  url: string;
 }
+
 export function Feature(props: FeatureProps) {
-  const { imageUrl, title, description } = props;
+  const { imageUrl, title, description, url } = props;
   const imgUrl = useBaseUrl(imageUrl);
 
+  const fontColor = 'var(--ifm-color-emphasis-800)';
+
   return (
-    <div className="border-man text--center">
-      {imgUrl && (
-        <div>
-          <img className={styles.featureImage} src={imgUrl} alt={title} />
-        </div>
-      )}
-      <h3>{title}</h3>
-      <Link>{description}</Link>
-    </div>
+    <Link to={url} style={{ height: '100%', verticalAlign: 'top'}}>
+      <Card
+        raised
+        style={{
+          backgroundColor: 'var(--ifm-color-emphasis-100)',
+          height: '100%',
+        }}
+      >
+        <CardActionArea style={{ height: '100%' }}>
+          <CardMedia
+            component="img"
+            image={imageUrl}
+            height="200px"
+            sx={{
+               paddingTop: '1.5em',
+               objectFit: 'contain',
+               verticalAlign: 'top',
+            }}
+          />
+          <CardContent>
+            <Typography
+              gutterBottom
+              align="center"
+              variant="h4"
+              component="div"
+              sx={{ color: fontColor }}
+            >
+              {title}
+            </Typography>
+            <Typography
+              variant="body1"
+              align="center"
+              sx={{ color: 'var(--ifm-color-emphasis-800)' }}
+            >
+              {description}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </Link>
   );
 }
