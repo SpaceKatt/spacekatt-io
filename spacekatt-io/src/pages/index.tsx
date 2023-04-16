bac6e499cdb0434be742a3175681c32c0e0e7826
SpaceKatt <spacekattpoispin@gmail.com>
2023-04-16 13:22:27 -0700

    fix index

--- spacekatt-io/src/pages/index.tsx
+++ spacekatt-io/src/pages/index.tsx
@@ -64,7 +64,7 @@ function Home() {
             <Box sx={{ flexGrow: 1, padding: 3 }}>
               <Grid2 container spacing={{ lg: 3, md: 2, xs: 1 }}>
                 {features.map((props, idx) => (
-                  <Grid2 xs={12} md={6} lg={4} xl={3} key={idx}>
+                  <Grid2 xs={12} md={6} lg={4} key={idx}>
                     <Box sx={{ padding: 3, height: '100%', minHeight: '400px' }}>
                       <Feature key={idx} {...props} ></Feature>
                     </Box>
