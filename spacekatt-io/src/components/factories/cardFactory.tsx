ab9d2c5e8e21b0e9d98db93bec26f1a7b57c2043
SpaceKatt <spacekattpoispin@gmail.com>
2023-04-15 23:55:31 -0700

    update card factory

--- spacekatt-io/src/components/factories/cardFactory.tsx
+++ spacekatt-io/src/components/factories/cardFactory.tsx
@@ -1,3 +1,6 @@
+import { Box } from '@mui/material';
+import Grid2 from '@mui/material/Unstable_Grid2';
+
 import React from 'react';
 import { ReactElement } from 'react';
 import { Card, CardProps } from '..';
@@ -9,13 +12,19 @@ export interface CardFactoryProps {
 export const CardFactory = (
   cardFactoryProps: CardFactoryProps,
 ): ReactElement => {
-  const cards = cardFactoryProps.cardProps.map(Card);
-
   return (
-    <div className="container">
-      <div className="row" style={{ gap: '50px' }}>
-        {cards}
-      </div>
-    </div>
+    <Box sx={{ flexGrow: 1, padding: 3 }}>
+      <Grid2 container spacing={{ lg: 3, md: 2, xs: 1 }}>
+        {cardFactoryProps.cardProps.map((prop, idx) => {
+          return (
+            <Grid2 xs={12} md={12} lg={6} xl={4} key={idx}>
+              <Box sx={{ padding: 3, height: '100%', minHeight: '400px' }}>
+                <Card {...prop} />
+              </Box>
+            </Grid2>
+          );
+        })}
+      </Grid2>
+    </Box>
   );
 };
