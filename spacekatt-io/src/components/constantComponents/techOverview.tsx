d31475abc012c291aeef8434e13235f801286e8f
SpaceKatt <spacekattpoispin@gmail.com>
2023-04-15 23:55:06 -0700

    add git repo doc to tech overview

--- spacekatt-io/src/components/constantComponents/techOverview.tsx
+++ spacekatt-io/src/components/constantComponents/techOverview.tsx
@@ -6,6 +6,15 @@ const techGalleryCardHeight = '250px';
 
 const content: CardProps[] = [
   {
+    header: 'Git Repo Setup Best Practices',
+    body: 'How-to implement best practices for setting up open source git repositories on GitHub.',
+    imgSrc: '/img/dispensesTheSquars.png',
+    alt: 'Meme of GitHub commit squars',
+    title: 'Git Repository Setup: Best Practices and How-to',
+    height: techGalleryCardHeight,
+    url: '/git-repo-best-practices',
+  },
+  {
     header: 'Weather Station',
     body: 'WiFi-connected temperature and humidity telemetry node, using an ESP32.',
     imgSrc: '/img/weather_station.png',
